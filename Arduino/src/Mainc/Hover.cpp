1a360e696ae184a0b564e38dc80309da96e19b4d
Torbjörn Sörman <torbjorn.sorman@gmail.com>
2014-05-13 17:59:59 +0200

    restructured class Hover and probably changed functionallity

--- Arduino/src/Mainc/Hover.cpp
+++ Arduino/src/Mainc/Hover.cpp
@@ -5,73 +5,83 @@ Hover::Hover(Motor *motors, sensordata *sensor, float refAltitude) {
 	init(motors, sensor, refAltitude);
 }
 
-void Hover::init(Motor *motors, sensordata *sensor, float refAltitude){
+void Hover::init(Motor *motors, sensordata *sensor, float refAltitude) {
+	/* Hardware */
 	this->motors = motors;
 	this->sensor = sensor;
-	this->time = micros();
+	timeStampMotor = timeStamp = micros();	
+
+	/* Desired position */
+	positionDesired[X] = 0;
+	positionDesired[Y] = 0;
+	positionDesired[Z] = refAltitude;
+
+	/* Starting position */
+	positionRelative[X] = 0;
+	positionRelative[Y] = 0;
+	positionRelative[Z] = 0;
+
+	/* Starting error in position */
+	positionErrorPrev[X] = 0;
+	positionErrorPrev[Y] = 0;
+	positionErrorPrev[Z] = refAltitude;
+
+	/* Initial motor speed, range 0 - 100 */
 	speed_lf = motors[leftfront].getSpeed();
 	speed_rf = motors[rightfront].getSpeed();
 	speed_lb = motors[leftback].getSpeed();
 	speed_rb = motors[rightfront].getSpeed();
-	this->refAltitude = refAltitude;
+
+	/* Calibration values */
 	angleCalibrateCount = 0;
-	angle_roll_offset = 0;
-	angle_pitch_offset = 0;
-	angle_yaw_offset = 0;
+	angleOffset[ROLL] = 0;
+	angleOffset[PITCH] = 0;
+	angleOffset[YAW] = 0;
+
+	leftFrontMotorZOld = 0;
+	rightFrontMotorZOld = 0;
+
+	/* Acceleration buffer values */
 	accelerationBufferCounter = 0;
-
-	old_lfmh = 0.0;
-	old_rfmh = 0.0;
-	old_lfmv = 0.0;
-	old_rfmv = 0.0;
-	old_lfma = 0.0;
-	old_rfma = 0.0;
-	old_cbh = 0.0;
-	old_cbv = 0.0;
-	old_cba = 0.0;
-	old_errorAltitude = 0.0;
-	int i = 0;
-	for (i = 0; i < sampleAlt; ++i)
-		meanAlt[i] = sensor->height;
-	cntAlt = 0;
-	initRefAlt = true;
-	calibrate_cnt = 0;
-	/* debug print out */
-	Serial.print("Reference altitude: ");
-	Serial.println(refAltitude);
 }
 
-bool Hover::Calibrate(void) 
+/* Buffers sensor values, angle buffer is used for the angle offset, 
+ * the acceleration buffer is used to avoid spikes when calculating 
+ * position from acceleration.
+ */
 
-	// Buffer up acceleration values
+bool Hover::Calibrate(void)  {
+
+	if (angleCalibrateCount == sampleAngle) return true;
+
+	// Buffer up acceleration values (sampleAcceleration / 3 times)
 	accelerationBuffer[accelerationBufferCounter++] = sensor->acc.x;
-accelerationBuffer[accelerationBufferCounter++] = sensor->acc.y;
-accelerationBuffer[accelerationBufferCounter] = sensor->acc.z;
-accelerationBufferCounter = ++accelerationBufferCounter % sampleAcceleration;
+	accelerationBuffer[accelerationBufferCounter++] = sensor->acc.y;
+	accelerationBuffer[accelerationBufferCounter] = sensor->acc.z;
+	accelerationBufferCounter = ++accelerationBufferCounter % sampleAcceleration;
 
-// Buffer up angle values
-angleCalibrate[angleCalibrateCount++] = sensor->angleRoll;
-angleCalibrate[angleCalibrateCount++] = sensor->anglePitch;
-angleCalibrate[angleCalibrateCount++] = sensor->angleYaw;
-if (angleCalibrateCount == sampleAngle) {
-	for (int i = 0; i < sampleAngle; ++i) {
-		angleOffset[Roll] += angleCalibrate[i];
-		angleOffset[Pitch] += angleCalibrate[++i];
-		angleOffset[Yaw] += angleCalibrate[++i];
+	// Buffer up angle values (sampleAngle / 3 times)
+	if (angleCalibrateCount == sampleAngle) {
+		for (int i = 0; i < sampleAngle; ++i) {
+			angleOffset[ROLL] += angleCalibrate[i];
+			angleOffset[PITCH] += angleCalibrate[++i];
+			angleOffset[YAW] += angleCalibrate[++i];
+		}
+		angleOffset[ROLL] /= (sampleAngle / 3);
+		angleOffset[PITCH] /= (sampleAngle / 3);
+		angleOffset[YAW] /= (sampleAngle / 3);
+		return true;
+	} else {
+		angleCalibrate[angleCalibrateCount++] = sensor->angleRoll;
+		angleCalibrate[angleCalibrateCount++] = sensor->anglePitch;
+		angleCalibrate[angleCalibrateCount++] = sensor->angleYaw;
 	}
-	angleOffset[Roll] /= (sampleAngle / 3);
-	angleOffset[Pitch] /= (sampleAngle / 3);
-	angleOffset[Yaw] /= (sampleAngle / 3);
-	return true;
-}
-return false;
+	return false;
 }
 
-/* Debug purpuse, calculate position relative start */
-
-void Hover::SimpleRegulate(void) {
+void Hover::Regulate(void) {
 	unsigned long timestampCurrent = micros();
-	unsigned long dt = timestampCurrent - time;
+	unsigned long dt = timestampCurrent - timeStamp;
 	unsigned long dtSqr = dt * dt;
 	float accX{0}, accY{0}, accZ{0};
 
@@ -87,118 +97,63 @@ void Hover::SimpleRegulate(void) {
 		accY += accelerationBuffer[++i];
 		accZ += accelerationBuffer[++i];
 	}
-	accX /= (sampleAcceleration / 3);
-	accY /= (sampleAcceleration / 3);
-	accZ /= (sampleAcceleration / 3);
 
-	/* Calculate position change */
-	position[x] += accX * dtSqr;
-	position[y] += accY * dtSqr;
-	position[z] += accZ * dtSqr;
+	/* Calculate relative position change */
+	positionRelative[X] += ((3 * accX) / sampleAcceleration) * dtSqr;
+	positionRelative[Y] += ((3 * accY) / sampleAcceleration) * dtSqr;
+	positionRelative[Z] += ((3 * accZ) / sampleAcceleration) * dtSqr;
 
-	/* Print relative position from start on the x axis once every second */
+	/* Debug prints to serial */	
 	if (dt > 1000000) {
 		Serial.print("X: ");
-		Serial.println(position[x]);
+		Serial.println(positionRelative[X]);
 		Serial.print("Y: ");
-		Serial.println(position[y]);
+		Serial.println(positionRelative[Y]);
 		Serial.print("Z: ");
-		Serial.println(position[z]);
+		Serial.println(positionRelative[Z]);
+		Serial.print("Left motor simulated speed: ");
+		Serial.println(speed_lf);
 	}
 
-	/* store time state */
-	time = timestampCurrent;
-}
-
-void Hover::Regulate(void) {
-
 	/* debug values, simplifies tests */
-	int     debug_maxEngineEffect = 50;
-	boolean debug_setEngineEffect = false;
+	int     debug_maxMotorEffect = 50;
+	boolean debug_setMotorEffect = false;
 
-	/* minimal interval between updates */
-	unsigned long minUpdateInterval = 200000;
+	/* Calculate new values for motors. 
+	 * OBS The value affects PD calculation -> dt = 1000
+	 */
+	if (timestampCurrent - timeStampMotor >= 1000){
 
-	/* retrieve current time stamp */
-	unsigned long currentTime = micros();
+		/* TODO map motors to axis correct */
+		float leftFrontMotorZ;
+		float rightFrontMotorZ;
+		float posError[3];
 
-	/* calculate elapsed time (us) */
-	unsigned long dt = currentTime - this->time;
+		/* retrieve motor altitudes relative centre position */
+		leftFrontMotorZ = motorDistanceCentre * tan(sensor->anglePitch);
+		rightFrontMotorZ = motorDistanceCentre * tan(sensor->angleRoll);
 
-	/* if dt exceeds minimal uptade interval, calculate new action */
-	if (dt > minUpdateInterval && !initRefAlt){
-
-		/* maximal error on altitude to put in the regulation algorithm */  
-		float maxErrorAltitude = 0.1;
-
-		/* naming scheme: position | [position2] | part | measure
-		* ex: lfmh = left front motor height
-		* ex: cbv = centre body velocity 
-		*/
-		float lfmh;
-		float rfmh;
-		float lfmv;
-		float rfmv;
-		float lfma;
-		float rfma;
-		float cbh;
-		float cbv;
-		//float cba;
-		float errorAltitude;
-
-		/* retrieve height */
-		lfmh = 0.4 * tan(sensor->anglePitch);
-		rfmh = 0.4 * tan(sensor->angleRoll);
-		//cbh = sensor->height;
-		int i = 0;
-		cbh = 0;
-		for(i = 0; i < sampleAlt; ++i)
-			cbh += meanAlt[i];
-		cbh = cbh / (float)sampleAlt;
-
-		/* retrieve velocity */
-		lfmv = (lfmh - old_lfmh) / dt;
-		rfmv = (rfmh - old_rfmh) / dt;
-		cbv = (cbh - old_cbh) / dt;
-
-		/* retrieve acceleration */
-		lfma = (lfmv - old_lfmv) / dt;
-		rfma = (rfmv - old_rfmv) / dt;
-		//cba = (cbv - old_cbv) / dt;
-
-		/* difference in meters from reference altitude (desired altitude) */
-		errorAltitude = refAltitude - cbh;
-
-		/* limit error to a maximal error altitude */
-		errorAltitude = (abs(errorAltitude) > maxErrorAltitude) ? (errorAltitude < 0 ? -maxErrorAltitude : maxErrorAltitude) : errorAltitude;
+		/* relative altitude difference from desired altitude */
+		posError[Z] = positionDesired[Z] - positionRelative[Z];
 
 		/* calculate how large alteration is taken on current speed. */
-		float cP = 10;
-		float cD = 100;
-		float dBodySpeed = errorAltitude * cP + ((errorAltitude - old_errorAltitude) / dt) * cD;
-		float cP_M = 10;
-		float cD_M = 100;
-		float dLeftFrontSpeed = lfmh * cP_M + ((lfmh - old_lfmh) / dt) * cD_M;
-		float dRightFrontSpeed = rfmh * cP_M + ((rfmh - old_rfmh) / dt) * cD_M;
+		// OBS short-cut since dt = 1000 then the d-part is shortened
+		float regPD = posError[Z] * 10.0 + (posError[Z] - positionError[Z]) * 0.1;
+		float regPDLM = leftFrontMotorZ * 10.0 + (leftFrontMotorZ - leftFrontMotorZOld) * 0.1;
+		float regPDRM = rightFrontMotorZ * 10.0 + (rightFrontMotorZ - rightFrontMotorZOld) * 0.1;
 
 		// DEBUG, do all changes equaly on all engines
-		dLeftFrontSpeed = 0.0;
-		dRightFrontSpeed = 0.0;
+		regLM = 0.0;
+		regRM = 0.0;
 
-		/* Set speed */
-		speed_lf = speed_lf + dBodySpeed + dLeftFrontSpeed;
-		speed_rf = speed_rf + dBodySpeed + dRightFrontSpeed;
-		speed_lb = speed_lb + dBodySpeed - dRightFrontSpeed;
-		speed_rb = speed_rb + dBodySpeed - dLeftFrontSpeed;
-
-		/* safety check, never set more then 50% of engine effect */    
-		speed_lf = min(speed_lf, debug_maxEngineEffect);
-		speed_rf = min(speed_rf, debug_maxEngineEffect);
-		speed_lb = min(speed_lb, debug_maxEngineEffect);
-		speed_rb = min(speed_rb, debug_maxEngineEffect);
+		/* Set speed, limited to a max value TODO have this mapped correct! */
+		speed_lf = min(speed_lf + regPD + regPDLM, debug_maxMotorEffect);
+		speed_rf = min(speed_rf + regPD + regPDRM, debug_maxMotorEffect);
+		speed_lb = min(speed_lb + regPD - regPDRM, debug_maxMotorEffect);
+		speed_rb = min(speed_rb + regPD - regPDLM, debug_maxMotorEffect);
 
 		/* set engine speed value from 0 to 100 */
-		if (debug_setEngineEffect) {
+		if (debug_setMotorEffect) {
 			motors[leftfront].setSpeed(speed_lf);
 			motors[rightfront].setSpeed(speed_rf);
 			motors[leftback].setSpeed(speed_lb);
@@ -206,43 +161,10 @@ void Hover::Regulate(void) {
 		}
 
 		/* store state */
-		old_lfmh = lfmh;
-		old_rfmh = rfmh;
-		old_cbh = cbh;
-		old_lfmv = lfmv;
-		old_rfmv = rfmv;
-		old_cbv = cbv;
-		old_lfma = lfma;
-		old_rfma = rfma;
-		//old_cba = cba;
-		old_errorAltitude = errorAltitude;
-		time = currentTime;
-
-		/* Debug prints */
-		/*Serial.println("=== new cycle ===");   
-		Serial.print("P: ");
-		Serial.println(errorAltitude * 10);
-		Serial.print("D: ");
-		Serial.println(((errorAltitude - old_errorAltitude) / dt) * 100);
-
-		Serial.print("Altitude: ");
-		Serial.println(cbh);
-
-		Serial.print("Velocity: ");
-		Serial.println(cbv);s    
-		Serial.print("Acceleration: ");
-		Serial.println(cba);
-
-		Serial.print("Current speed (lf, [rf, lb, rb]): ");
-		Serial.println(speed_lf);
-
-		Serial.println(speed_rf);
-		Serial.println(speed_lb);
-		Serial.println(speed_rb);
-		Serial.print("Last speed changes (body, lf, rf): ");
-		Serial.println(dBodySpeed);
-		Serial.println(dLeftFrontSpeed);
-		Serial.println(dRightFrontSpeed);
-		*/
+		leftFrontMotorZOld = leftFrontMotorZ;
+		rightFrontMotorZOld = rightFrontMotorZ;
+		positionErrorOld = positionError;
+		timeStampMotor = timestampCurrent;
 	}  
+	timeStamp = timeStampCurrent;
 }
--- Arduino/src/Mainc/Hover.h
+++ Arduino/src/Mainc/Hover.h
@@ -5,23 +5,35 @@
 #include "SensorDataStruct.h"
 
 #define sampleAngle 24        // OBS must be divisible by three
-#define sampleAcceleration 24 // OBS must be divisible by three
+#define sampleAcceleration 9  // OBS must be divisible by three
+
+#define motorDistanceCentre 0.4
+
+/* OBSERVER!
+ * How to run this regulator:
+ * 1. Run init
+ * 2. Run Calibrate until it returns true
+ * 3. Run Regulate as many times regulation is needed
+ */
+
 
 class Hover {
 private:
 
-	float average(float buffer, int cnt);
+	enum Angle { ROLL = 0, PITCH = 1, YAW = 2 };
+	enum Position { X = 0, Y = 1, Z = 2 };
 
 	/* Hardware */
 	Motor *motors;
 	sensordata *sensor;
 
-	/* Reference altitude or desired altitude from starting position */
-	float refAltitude;
-	unsigned long time;
+	/* last call and last motor update */
+	unsigned long timeStamp, timeStampMotor;
 
 	/* Internal position from start*/
-	float position[3];
+	float positionRelative[3];
+	float positionDesired[3];
+	float positionErrorPrev[3];
 
 	/* calibration of angle */
 	int angleCalibrateCount;
@@ -34,32 +46,20 @@ private:
 	int accelerationBufferCounter;
 	float accelerationBuffer[sampleAcceleration]; // # sample * (x, y, z)
 
-	float old_lfmh;
-	float old_rfmh;
-	float old_lfmv;
-	float old_rfmv;
-	float old_lfma;
-	float old_rfma;
-	float old_cbh;
-	float old_cbv;
-	float old_cba;
-	float old_errorAltitude;
+	float leftFrontMotorZOld;
+	float rightFrontMotorZOld;
 
 	/* Internal speed when debugging without real engines */
 	float speed_lf;
 	float speed_rf;
 	float speed_lb;
-	float speed_rb;
-
-	enum Angle { Roll = 0, Pitch = 1, Yaw = 2 };
-	enum Position { x = 0, y = 1, z = 2 };
+	float speed_rb;	
 
 public:
 	Hover() {};
 	Hover(Motor *motors, sensordata *sensor, float refAltitude);
 	void init(Motor *motors, sensordata *sensor, float refAltitude);
 	bool Calibrate(void);
-	void SimpleRegulate(void);
 	void Regulate(void);
 };
 
