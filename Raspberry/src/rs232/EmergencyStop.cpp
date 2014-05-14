abfe26d244bbc4c1cc2b59307b8a8dd41bc8715d
ramboerik <eriktideman@gmail.com>
2014-05-14 22:58:51 +0200

    Added emergency stop function to the arduino

---
+++ Raspberry/src/rs232/EmergencyStop.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <wiringSerial.h>
+
+using namespace std;
+
+int main(){
+
+  int fd = serialOpen ("/dev/ttyAMA0", 115200);
+  char bufferData[100];
+
+  if(fd == -1){
+    cout << "Cannot open the serial port, try root?" << endl;
+    return 0;
+  }
+
+  cout << "Listening after data on serial port..." << endl;
+  while(true){
+    serialPutchar(fd, '\n'); 
+    while(serialDataAvail(fd)){
+      cout << (char)serialGetchar(fd);
+    }
+  }
+
+  serialClose(fd);
+  return 0;
+}
