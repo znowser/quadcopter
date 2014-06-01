73abbd2daba090a0e1c083794c7099db4e523227
torbjorn-sorman <torbjorn.sorman@gmail.com>
2014-06-01 16:19:34 +0200

    Nicht gut
    FIFO overflow... och chansreglering

--- Arduino/src/Mainc/Hover.h
+++ Arduino/src/Mainc/Hover.h
@@ -13,20 +13,21 @@
 #define axisPi 4
 #define axisYa 5
 
-#define MOTOR_MAX 50
-#define MOTOR_MIN 20
+#define MOTOR_MAX 60
+#define MOTOR_MIN 10
 #define accDeadzone 32      
 #define angDeadzone 4
 
 #define COLD_START 1024       // Number of iteration before actual calibration
 #define CALIBRATION_CNT 256   // Number of sampled values during calibration
-#define SAMPLE_CNT 8          // Samples for average values
+#define SAMPLE_CNT 4          // Samples for average values
 
 #define VELOCITY_REDUCE 0.66f // Velocity must always reduced at every postion calc. 
 
-#define MAX_RUNTIME 10        // Number of second the test should last
-#define SPEED_UP_LIM 40       // Number of +1 speed increase/second (speed decreases rest of time) lift off > 44
-#define START_SPEED 34        // Motor start speed
+#define MAX_RUNTIME 20        // Number of second the test should last
+#define SPEED_UP_LIM 55      // Number of +1 speed increase/second (speed decreases rest of time) lift off > 44
+#define START_SPEED 40       // Motor start speed
+
 
 // Current config will let the quad accelerate from 20 to 30 and then down to 20.
 // Idea is to test the top value intil lift of :-)
@@ -50,10 +51,10 @@ private:
   //long a[3][2], v[3][2], p[3][2], pRef[3];
   long smp[6], sstate[6];
   /* PD */
-  int e[6], eOld[6], u[6];
+  float e[6], eOld[6], u[6];
   float K[6], Td[6];
   /* MOTOR EFFECT */
-  int speed[4];
+  float speed[4];
 
 public:
   Hover(Motor *motors, sensordata *sensor, float refAltitude);
