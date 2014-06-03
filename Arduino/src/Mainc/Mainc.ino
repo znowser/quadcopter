881e61e2150804f6ba4866a4157320bb699a0204
ramboerik <eriktideman@gmail.com>
2014-06-03 19:47:25 +0200

    Main now uses slower implementation of read pressure/temp, this is only temporary

--- Arduino/src/Mainc/Mainc.ino
+++ Arduino/src/Mainc/Mainc.ino
@@ -8,6 +8,7 @@
 #include "Hover.h"
 //#include "SerialBuss.h"
 #include "BussProtocol.h"
+#include "vector_math.h"
 
 //change this variable to true if you want to turn on the regulator, Torbjörn.
 const bool regulator_activated = true;
@@ -24,6 +25,8 @@ void loop() {
 /*========================================*/
 
 float getAltitude(float press, float temp);
+float getEstimatedAlt(float height);
+float invSqrt(float number);
 void updateSensorValues(sensordata &sensorData, Motor motor[4], CellVoltage battery[3], MS561101BA &baro, float ypr[3]);
 //cannot be called with the name: main(), strange Arduino syndrome!
 int mainf() {
@@ -54,6 +57,8 @@ int mainf() {
   battery[CELL2].init(A1);
   battery[CELL3].init(A2);
   /*==================================*/
+  
+  delay(1000);
 
   //serial.registerCallback(ps3DataCallback, &sensorData, PS3_CONTROLLER_PACKAGE);
   /*==========Hover regulator=============*/
@@ -64,24 +69,36 @@ int mainf() {
   int sendCnt = 0;
   while (true) {
     //TODO continue to implement the new buss protocol
-  //  serial.recvRasp();
+    //  serial.recvRasp();
 
     //check if there is new sensordata to recieve from the sensor card
     if (mpu.readYawPitchRoll(ypr, sensorData.acc)) {
       //update sensor struct
       updateSensorValues(sensorData, motor, battery, baro, ypr);
-      Serial.print("sensor freq: ");
-      Serial.println(++sendCnt / (micros()/ 1000000.f));
-      /*if (++sendCnt % 10 == 0) {
+      
+      //Serial.println("================");
+      /*Serial.print("Pressure ");
+      Serial.print(sensorData.pressure / 100.0);
+      Serial.print("   temp ");
+      Serial.print(sensorData.temperature / 100.0);
+      Serial.print("   Height: ");
+      Serial.println(sensorData.height);*/
+      //Serial.println("================");
+      
+     // Serial.print("Fused height: ");
+      //Serial.println(getEstimatedAlt(sensorData.height));
+      //Serial.print("sensor freq: ");
+      //Serial.println(++sendCnt / (micros() / 1000000.f));
+      if (++sendCnt % 10 == 0) {
         Serial.write(buildSensorPackage(sensorData, tmp, len), len);
         Serial.println();
-      }*/
+      }
       //send the sensorstruct to the raspberry or regulate
       /*if (regulator_activated && regulator.Calibrate())
         regulator.Regulate();
         */
-   //   else
-   //     serial.sendRasp(SENSORDATA_PACKAGE, buildSensorPackage(sensorData, tmp, len), len);
+      //   else
+      //     serial.sendRasp(SENSORDATA_PACKAGE, buildSensorPackage(sensorData, tmp, len), len);
     }
   }
 
@@ -93,12 +110,21 @@ int mainf() {
 float getAltitude(float press, float temp) {
   //return (1.0f - pow(press/101325.0f, 0.190295f)) * 4433000.0f;
   return ((pow((sea_press / press), 1 / 5.257) - 1.0) * (temp + 273.15)) / 0.0065;
+  
+ // -KT*ln(Pn/P0)/mg = h
+ // K boltzmann's constant
+ // T Temperature in kelvin
+ // Pn current pressure
+ // P0 sea pressure
+ // m Mass of one molecule = 29.95 (average of air)
+ // g 9.8
+ //-(temp + 273.15)*1.3806488*ln(press/sea_press)
 }
 
 void updateSensorValues(sensordata &sensorData, Motor motor[4], CellVoltage battery[3], MS561101BA &baro, float ypr[3]) {
   //temperature and pressure cannot be read directly after each other, it
   //must be a small delay between the two functioncalls.
-  sensorData.temperature = baro.getTemperature(MS561101BA_OSR_4096);
+  //sensorData.temperature = baro.getTemperature(MS561101BA_OSR_4096);
   //convert from radians to degrees
   sensorData.angleYaw = ypr[0]  * 180 / M_PI;
   sensorData.anglePitch = ypr[1]  * 180 / M_PI;
@@ -113,10 +139,95 @@ void updateSensorValues(sensordata &sensorData, Motor motor[4], CellVoltage batt
   sensorData.motorSpeed[RB] = motor[RB].getSpeed();
   //get pressure, cannot be done directly after getTemperature, The sensorcard need
   //a small delay between the function calls.
-  sensorData.pressure = baro.getPressure(MS561101BA_OSR_4096);
-  sensorData.height = getAltitude(sensorData.pressure, sensorData.temperature);
+  //baro.getData(sensorData.temperature, sensorData.pressure, MS561101BA_OSR_4096);
+  baro.getData(sensorData.temperature, sensorData.pressure, MS561101BA_OSR_256);
+  //sensorData.pressure = baro.getPressure(MS561101BA_OSR_4096);
+  sensorData.height = getAltitude(sensorData.pressure / 100.0, sensorData.temperature / 100.0);
 }
 
+// // complementary filter from MultiWii project v1.9
+//
+ #define UPDATE_INTERVAL 25000    // 40hz update rate (20hz LPF on acc)
+ #define INIT_DELAY      4000000  // 4 sec initialization delay
+ #define Kp1 0.55f                // PI observer velocity gain
+ #define Kp2 1.0f                 // PI observer position gain
+ #define Ki  0.001f               // PI observer integral gain (bias cancellation)
+ #define dt  (UPDATE_INTERVAL / 1000000.0f)
+//
+// /**
+// * Returns an altitude estimate from baromether fused with accelerometer readings
+// */
+float getEstimatedAlt(float height) {
+  static uint8_t inited = 0;
+  static int16_t AltErrorI = 0;
+  static float AccScale  = 0.0f;
+  static uint32_t deadLine = INIT_DELAY;
+  int16_t AltError;
+  int16_t InstAcc;
+  int16_t Delta;
+  static int32_t  EstVelocity;
+  static int32_t  EstAlt;
+
+  long currentTime = micros();
+
+  //if (currentTime < deadLine) return EstAlt;
+  //deadLine = currentTime + UPDATE_INTERVAL;
+  // Soft start
+Serial.print("Height : ");
+Serial.println(height);
+  if (!inited) {
+    inited = 1;
+    EstAlt = height;
+    EstVelocity = 0;
+    AltErrorI = 0;
+  }
+  // Estimation Error
+  AltError = height - EstAlt;
+  AltErrorI += AltError;
+  AltErrorI = constrain(AltErrorI, -25000, +25000);
+  // Gravity vector correction and projection to the local Z
+  //InstAcc = (accADC[YAW] * (1 - acc_1G * InvSqrt(isq(accADC[ROLL]) + isq(accADC[PITCH]) + isq(accADC[YAW])))) * AccScale + (Ki) * AltErrorI;
+#if defined(TRUSTED_ACCZ)
+  InstAcc = (accADC[YAW] * (1 - acc_1G * InvSqrt(isq(accADC[ROLL]) + isq(accADC[PITCH]) + isq(accADC[YAW])))) * AccScale +  AltErrorI / 1000;
+#else
+  InstAcc = AltErrorI / 1000;
+#endif
+
+  // Integrators
+  Delta = InstAcc * dt + (Kp1 * dt) * AltError;
+//  EstAlt += (EstVelocity / 5 + Delta) * (dt / 2) + (Kp2 * dt) * AltError;
+  EstVelocity += Delta * 10;
+
+
+ // vmath::quat<float> ciao = vmath::quat<float>(0.0, 0.1, 0.2, 0.3);
+  //vmath::quat<float> ciao2;
+
+ // ciao = ciao * ciao2;
+
+
+  //Serial.print(":::");
+  //Serial.println(ciao.w);
+
+  return EstAlt;
+}
+
+/**
+ * Fast inverse square root implementation
+ * @see http://en.wikipedia.org/wiki/Fast_inverse_square_root
+*/
+float invSqrt(float number) {
+  volatile long i;
+  volatile float x, y;
+  volatile const float f = 1.5F;
+
+  x = number * 0.5F;
+  y = number;
+  i = * ( long * ) &y;
+  i = 0x5f375a86 - ( i >> 1 );
+  y = * ( float * ) &i;
+  y = y * ( f - ( x * y * y ) );
+  return y;
+}
 
 
 
