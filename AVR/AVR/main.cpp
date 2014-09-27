8566efc37a0baa6543ca196d74958f122a015aa0
torbjorn-sorman <torbjorn.sorman@gmail.com>
2014-09-27 11:21:53 +0200

    Build succeded

--- AVR/AVR/main.cpp
+++ AVR/AVR/main.cpp
@@ -14,8 +14,11 @@
 #include "HoverRegulator/Hover.h"
 
 const bool regulator_activated = true;
-float getAltitude(float press, float temp);
-void updateSensorValues(sensordata &sensorData, Motor motor[4], CellVoltage battery[3], MS561101BA &baro, float ypr[3]);
+float getAltitude(sensordata &sensor, float press);
+void updateSensorValues(sensordata &sensor, Motor motor[4], CellVoltage battery[3], MS561101BA &baro, float ypr[3]);
+void initSensorValues(sensordata &sensor, MS561101BA &baro);
+void initAltitudeMeasurement(sensordata &sensor, float ref_pres, float ref_temp);
+
 int main(void)
 {
 	//initial all pins according to arduino standard
@@ -25,7 +28,7 @@ int main(void)
 	
 	Motor motor[4];
 	CellVoltage battery[3];
-	sensordata sensorData;
+	sensordata sensor;
 	float ypr[3];
 	/*==========Init Sensors============*/
 	//init gyro and magnetic field
@@ -53,16 +56,16 @@ int main(void)
 	
 	/*==========Hover regulator=============*/
 	float refVal[6] = { 0, 0, 1.0, 0, 0, 0 };
-	Hover regulator(motor, &sensorData, refVal);
-	initSensorValues(sensorData, baro);
+	Hover regulator(motor, &sensor, refVal);
+	initSensorValues(sensor, baro);
 	static byte printAltInterval = 0;
     while(true){
 		//check if there is new sensordata to recieve from the sensor card
-		if (mpu.readYawPitchRoll(ypr, sensorData.acc)) {
+		if (mpu.readYawPitchRoll(ypr, sensor.acc)) {
 		//	//update sensor struct
-			updateSensorValues(sensorData, motor, battery, baro, ypr);
+			updateSensorValues(sensor, motor, battery, baro, ypr);
 			if (!(++printAltInterval % 8))
-				Serial1.println(sensorData.height);
+				Serial1.println(sensor.height);
 		/*
 			Serial1.print("Battery level: ");
 			Serial1.print(battery[CELL1].getVoltage());
@@ -79,11 +82,11 @@ int main(void)
 			Serial1.println(getAltitude(press, temp));
 		
 			Serial1.print("Yaw ");
-			Serial1.print(sensorData.gyro[0]);
+			Serial1.print(sensor.gyro[0]);
 			Serial1.print(" Pitch ");
-			Serial1.print(sensorData.gyro[1]);
+			Serial1.print(sensor.gyro[1]);
 			Serial1.print(" Roll ");
-			Serial1.println(sensorData.gyro[2]);
+			Serial1.println(sensor.gyro[2]);
 		*/
 		//  if (regulator_activated)
 		//	regulator.Regulate();
@@ -106,46 +109,47 @@ int main(void)
     }
 }
 
-float getAltitude(float pressure) {
-	return sensordata.alt.c1(sensordata.alt.c2 - log(press));
-}
-
-void updateSensorValues(sensordata &sensorData, Motor motor[4], CellVoltage battery[3], MS561101BA &baro, float ypr[3]) {
+void updateSensorValues(sensordata &sensor, Motor motor[4], CellVoltage battery[3], MS561101BA &baro, float ypr[3]) {
 	static byte batInterval = 255;
 	// Battery Cell 1
 	if (!++batInterval)
-		sensorData.cellVoltage[CELL1] = battery[CELL1].getVoltage();
+		sensor.cellVoltage[CELL1] = battery[CELL1].getVoltage();
 	// Gyro
-	sensorData.gyro[2] = ypr[0];
-	sensorData.gyro[1] = ypr[1];
-	sensorData.gyro[0] = ypr[2];
+	sensor.gyro[2] = ypr[0];
+	sensor.gyro[1] = ypr[1];
+	sensor.gyro[0] = ypr[2];
 	// Battery Cell 2
 	if (!batInterval)
-		sensorData.cellVoltage[CELL2] = battery[CELL2].getVoltage();	
+		sensor.cellVoltage[CELL2] = battery[CELL2].getVoltage();	
 	// Motor	
-	sensorData.motorSpeed[LF] = motor[LF].getSpeed();
-	sensorData.motorSpeed[RF] = motor[RF].getSpeed();
-	sensorData.motorSpeed[LB] = motor[LB].getSpeed();
-	sensorData.motorSpeed[RB] = motor[RB].getSpeed();
+	sensor.motorSpeed[LF] = motor[LF].getSpeed();
+	sensor.motorSpeed[RF] = motor[RF].getSpeed();
+	sensor.motorSpeed[LB] = motor[LB].getSpeed();
+	sensor.motorSpeed[RB] = motor[RB].getSpeed();
 	// Battery Cell 3
 	if (!batInterval)
-		sensorData.cellVoltage[CELL3] = battery[CELL3].getVoltage();
+		sensor.cellVoltage[CELL3] = battery[CELL3].getVoltage();
 	// Barometer
-	sensorData.pressure = baro.getPressure(MS561101BA_OSR_4096);
-	sensorData.height = getAltitude(sensorData.pressure);
+	sensor.pressure = baro.getPressure(MS561101BA_OSR_4096);
+	sensor.height = getAltitude(sensor, sensor.pressure);
 }
 
-void initSensorValues(sensordata &sensorData, MS561101BA &baro) {
+float getAltitude(sensordata &sensor, float pressure) {
+	return sensor.alt.c1*(sensor.alt.c2 - log(pressure));
+}
+
+void initSensorValues(sensordata &sensor, MS561101BA &baro) {
 	// Barometer
-	sensorData.temperature = baro.getTemperature(MS561101BA_OSR_4096);
+	sensor.temperature = baro.getTemperature(MS561101BA_OSR_4096);
 	// Delay readings, barometer
 	delay(100);
-	sensorData.pressure = baro.getPressure(MS561101BA_OSR_4096);
+	sensor.pressure = baro.getPressure(MS561101BA_OSR_4096);
 	// Init reference for altitude measurement
-	initAltitudeMeasurement(sensorData.pressure, sensorData.temperature);
+	initAltitudeMeasurement(sensor, sensor.pressure, sensor.temperature);
 }
 
-void initAltitudeMeasurement(float ref_pres, float ref_temp) {
-	sensordata.alt.c1 = (sensordata.alt.dryAirGasConst / sensordata.alt.gravAcc) * ref_temp;
-	sensordata.alt.c2 = log(ref_pres);
+void initAltitudeMeasurement(sensordata &sensor, float ref_pres, float ref_temp) {
+	sensor.alt.c1 = (sensor.alt.dryAirGasConst / sensor.alt.gravAcc) * ref_temp;
+	sensor.alt.c2 = log(ref_pres);
 }
+
