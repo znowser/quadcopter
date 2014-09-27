8e3801d60a58feec82bb6db07c80d281237f8474
ramboerik <eriktideman@gmail.com>
2014-09-27 17:42:44 +0200

    final working release

--- AVR/AVR/main.cpp
+++ AVR/AVR/main.cpp
@@ -8,11 +8,22 @@
 #include "lib/Arduino/Arduino.h"
 
 int main(void) {
+	//initial all pins according to arduino standard !! MUST BE DONE !!
+	init();
+	
 	Serial1.begin(115200);
-
 	Serial1.println("Beginning test");
+	
+	unsigned long itr = 0;
+	unsigned long start = micros();
 	while(true){
-		Serial1.println(" Test print ");
-		delay(200);
+		if(itr % 100000 == 0){
+			Serial1.print("Iterations per second: ");
+			Serial1.println(itr / ((micros() - start) / 1000000));
+		}
+		//prevent overflow error
+		if(++itr == 0)
+			start = micros();
 	}
+	return 0;
 }
