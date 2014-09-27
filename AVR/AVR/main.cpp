8265598cb5751b052934454fb7193f662665fb19
ramboerik <eriktideman@gmail.com>
2014-09-27 18:18:59 +0200

    final commit, everything works

--- AVR/AVR/main.cpp
+++ AVR/AVR/main.cpp
@@ -8,7 +8,7 @@
 #include "lib/Arduino/Arduino.h"
 #include "FreeIMU/MS561101BA.h"
 
-/*
+
 #define MOVAVG_SIZE 32
 
 float movavg_buff[MOVAVG_SIZE];
@@ -18,42 +18,41 @@ float press, temperature;
 
 float getAltitude(float press, float temp);
 void pushAvg(float val);
-float getAvg(float *buff, int size);*/
+float getAvg(float *buff, int size);
 int main(void) {
+	//init arduino pins !!MUST BE DONE!!
+	init();
 	Wire.begin();
 	Serial1.begin(115200);
-	//delay(1000);
+	delay(1000);
 
-	Serial1.println("Beginning ");
 	// Suppose that the CSB pin is connected to GND.
 	// You'll have to check this on your breakout schematics
-	//MS561101BA baro = MS561101BA(MS561101BA_ADDR_CSB_LOW);
-	//delay(100);
+	MS561101BA baro = MS561101BA(MS561101BA_ADDR_CSB_LOW);
+	delay(100);
 		
 	// populate movavg_buff before starting loop
-	//for(int i=0; i<MOVAVG_SIZE; i++) {
-	//	movavg_buff[i] = baro.getPressure(MS561101BA_OSR_4096);
-	//}
+	for(int i=0; i<MOVAVG_SIZE; i++) {
+		movavg_buff[i] = baro.getPressure(MS561101BA_OSR_4096);
+	}
 	
 	while(true){
-		Serial1.print(" temp: ");
-		delay(100);
-		/*temperature = baro.getTemperature(MS561101BA_OSR_4096);
-		Serial1.print(temperature);
-		Serial1.print(" degC pres: ");
+		//Serial1.print(" temp: ");
+		temperature = baro.getTemperature(MS561101BA_OSR_4096);
+		//Serial1.print(temperature);
+		//Serial1.print(" degC pres: ");
 	
 		press = baro.getPressure(MS561101BA_OSR_4096);
 		pushAvg(press);
 		press = getAvg(movavg_buff, MOVAVG_SIZE);
-		Serial1.print(press);
-		Serial1.print(" mbar altitude: ");
-		Serial1.print(getAltitude(press, temperature));
-		Serial1.println(" m");
-		*/
+		//Serial1.print(press);
+		//Serial1.print(" mbar altitude: ");
+		Serial1.println(getAltitude(press, temperature));
+		//Serial1.println(" m");
 	}
 }
 
-/*
+
 float getAltitude(float press, float temp) {
 	//return (1.0f - pow(press/101325.0f, 0.190295f)) * 4433000.0f;
 	return ((pow((sea_press / press), 1/5.257) - 1.0) * (temp + 273.15)) / 0.0065;
@@ -70,5 +69,3 @@ float getAvg(float * buff, int size) {
 		sum += buff[i];
 	return sum / size;
 }
-
-*/
